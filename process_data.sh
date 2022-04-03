ace853de6639d6daae5b2ddacbe1992ce7eb88ae
Mikołaj Pokrywka <mp7961@gmail.com>
2022-04-03 15:54:50 +0200

    r

--- process_data.sh
+++ process_data.sh
@@ -4,8 +4,7 @@ echo $KAGGLE_USERNAME
 kaggle datasets download -d shivamb/real-or-fake-fake-jobposting-prediction
 unzip -o real-or-fake-fake-jobposting-prediction.zip
 ls
-mkdir data
-cp fake_job_postings.csv /data/fake_job_postings.csv
+cat fake_job_postings.csv > data.csv
 echo "Save column titles"
 head -n 1 fake_job_postings.csv > column_titles.csv
 tail -n +2 fake_job_postings.csv > data_not_shuf.csv
